3d9483a41273d0518a37fbab87ec5690bc0f81d2
leaf-junkie <ragsdale.jar@gmail.com>
2021-02-03 22:23:15 -0500

    Implement page routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
+import HomePage from './components/HomePage/HomePage';
+import ContactPage from './components/ContactPage/ContactPage';
 import './App.css';
 
-function App() {
-  return (
-    <div className="container">
-      <div className="logo">logo</div>
-      <div className="nav">nav</div>
-      <div className="hero">hero</div>
-      <div className="content">content</div>
-      <div className="footer">footer</div>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      newMessage: false
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Nav/>
+        <div>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={() => <HomePage/>} />
+              <Route exact path="/HomePage" component={() => <HomePage/>} />
+              <Route exact path="/ContactPage" component={() => <ContactPage/> } />
+            </Switch>
+          </Router>
+        </div>
+        <Footer/>
+      </div>
+    );
+  }
 }
 
 export default App;
