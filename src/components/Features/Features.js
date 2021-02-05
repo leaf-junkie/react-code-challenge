ae730cef895e8289787a4a736c8bd68a96a4417b
leaf-junkie <ragsdale.jar@gmail.com>
2021-02-04 22:33:12 -0500

    Begin to structure Features section

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,19 +1,31 @@
-import React from "react";
-import "./Features.css";
+import React from 'react';
+import './Features.css';
 
 function Features() {
   return (
-    <section className="features">
-        <div className="feature">
-            <p>Feature 1</p>
+    <>
+    <section className='banner'>
+      <h2>Features</h2>
+    </section>
+    <section className='features'>
+        <div className='feature'>
+          <h3>Feature 1</h3>
+          <p>
+            Authentic helvetica gastropub subway tile bitters.
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
         </div>
-        <div>
-            <p>Feature 2</p>
+        <div className='feature'>
+          <h3>Feature 2</h3>
+          <p> 
+          Lorem occaecat elit heirloom.
+          </p>
         </div>
-        <div>
-            <p>Feature 3</p>
+        <div className='feature'>
+          <h3>Feature 3</h3>
         </div>
     </section>
+    </>
   );
 }
 
