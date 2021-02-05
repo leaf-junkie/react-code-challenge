a5f64cec996f0fe8d74c5cd9095b2c12eb5ab094
leaf-junkie <ragsdale.jar@gmail.com>
2021-02-04 22:34:26 -0500

    Add and rearrange components

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
+import Hero from '../Hero/Hero';
 import Features from '../Features/Features';
-import Reviews from '../Reviews/Reviews';
 
 class HomePage extends Component {
 
     render() {
         return (
+            <>
+            <Hero/>
             <main>
-                <h2>homepage</h2>
                 <Features/>
-                <Reviews/>
             </main>
+            </>
         )
     }
 }
 
 export default HomePage;
+ 
