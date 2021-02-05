91f17259029aafd700294d7a7042a4231200a084
leaf-junkie <ragsdale.jar@gmail.com>
2021-02-04 22:35:05 -0500

    Add logo

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import './Nav.css';
+import Logo from '../../assets/Logo.png'
 
 const Nav = () => {
     return (
         <nav>
+            <a href='/HomePage'><img className='logo' src={Logo}/></a>
             <a href='/HomePage'>Home</a>
             <a href='/ContactPage'>Contact</a>
         </nav>
