8e92e2338a6d3c818698554274690489188bdfd4
leaf-junkie <ragsdale.jar@gmail.com>
2021-02-04 22:31:57 -0500

    Apply image background. Create layout with text and image.

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Hero.css';
+import Drone from './Drone.png';
+
+function Hero () {
+    return (
+        <header className='hero'>
+            <div className='heroText'>
+                <h1 className='display'>
+                    F-210
+                </h1>
+                <h4>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+                    enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                    nisi ut aliquip ex ea commodo consequat.
+                </h4>
+                
+            </div>
+            <img className='drone' src={Drone} alt='Drone'/>
+        </header>
+    );
+}
+
+export default Hero;
